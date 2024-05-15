69adb9eb793c4d0943729a68b924d8ddff12165e
Snow-00 <wariorwilly43@gmail.com>
2024-05-15 19:56:14 +0700

    finish get gempa info

--- cmd/bmkg/bmkg.go
+++ cmd/bmkg/bmkg.go
@@ -8,18 +8,35 @@ import (
 	"net/http"
 )
 
-type Response struct {
-	Infogempa struct{}
+type Gempa struct {
+	Tanggal     string `json:"Tanggal"`
+	Jam         string `json:"Jam"`
+	Coordinates string `json:"Coordinates"`
+	Magnitude   string `json:"Magnitude"`
+	Kedalaman   string `json:"Kedalaman"`
+	Wilayah     string `json:"Wilayah"`
+	Potensi     string `json:"Potensi"`
+	Dirasakan   string `json:"Dirasakan"`
+	Shakemap    string `json:"Shakemap"`
 }
 
+type Response struct {
+	Infogempa struct {
+		Gempa Gempa `json:"gempa"`
+	} `json:"Infogempa"`
+}
+
+// this is link for get image
+// const image = "https://data.bmkg.go.id/DataMKG/TEWS/${Shakemap}?000"
+
 func main() {
-	res, err := http.Get("https://data.bmkg.go.id/DataMKG/TEWS/autogempa.json?000")
+	resp, err := http.Get("https://data.bmkg.go.id/DataMKG/TEWS/autogempa.json?000")
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer resp.Body.Close()
 
-	body, err := io.ReadAll(res.Body)
-	res.Body.Close()
+	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
